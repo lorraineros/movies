f57fcf92854fde5216cdaade8974d2a1e40087cf
lorraineros <lorrainert2002@gmail.com>
2024-06-07 20:41:54 +0000

    Update Search.js

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -6,9 +6,11 @@ export default function Search() {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [searchExecuted, setSearchExecuted] = useState(false);
 
   const handleSearch = (e) => {
     e.preventDefault();
+    setSearchExecuted(true);
     if (query && query.length > 0) {
       setLoading(true);
       searchMovies({
@@ -37,7 +39,7 @@ export default function Search() {
           type='text'
           value={query}
           onChange={handleChange}
-          placeholder='Search for a movie...'
+          placeholder='Search'
           className='p-2 pl-4 pr-10 border rounded w-full'
         />
         <button
@@ -78,7 +80,7 @@ export default function Search() {
             </div>
           </div>
         ) : (
-          <div className='flex justify-center items-center m-10'>
+          <><div className='flex justify-center items-center m-10'>
             <svg 
               xmlns='http://www.w3.org/2000/svg' 
               viewBox='0 0 512 512'
@@ -89,6 +91,8 @@ export default function Search() {
                 d='M448 32H361.9l-1 1-127 127h92.1l1-1L453.8 32.3c-1.9-.2-3.8-.3-5.8-.3zm64 128V96c0-15.1-5.3-29.1-14-40l-104 104H512zM294.1 32H201.9l-1 1L73.9 160h92.1l1-1 127-127zM64 32C28.7 32 0 60.7 0 96v64H6.1l1-1 127-127H64zM512 192H0V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192z'/>
             </svg>
           </div>
+          {searchExecuted ? <div className='text-xl text-center my-10'>No results found</div> : <div className='text-xl text-center my-10'>Search for a movie...</div>}
+          </>
         )
       )}
     </div>
